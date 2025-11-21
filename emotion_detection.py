67ecd0c5389db5bf6e16692bfac42aabc87cc681
Gavin <gavin.furtado12@gmail.com>
2025-11-21 09:26:16 +0530

    emotion scores

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,20 +1,31 @@
-import json
-from typing import Any
 import requests
+import json
 
-def emotion_detector(text_to_analyse: str):  
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj: dict[str, dict[str, str]] = { "raw_document": { "text": text_to_analyse } }
-    response = requests.post(url, json = myobj, headers=header)
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-    return response.text
+    payload = {"raw_document": {"text": text_to_analyze}}
 
-    
+    response = requests.post(url, json=payload, headers=headers)
+    formatted_resp = json.loads(response.text)
 
-    formatted_response: Any = json.loads(response.text)
+    emotions = formatted_resp["emotionPredictions"][0]["emotion"]
 
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    emotion_scores = {
+        "anger_score": emotions["anger"],
+        "disgust_score": emotions["disgust"],
+        "fear_score": emotions["fear"],
+        "joy_score": emotions["joy"],
+        "sadness_score": emotions["sadness"]
+    }
 
-    return { 'label': label, 'score': score }
+    dominant_emotion = None
+    highest_score = -1 # lowest possible value
+    for emotion, value in emotion_scores.items():
+        if value > highest_score:
+            highest_score = value
+            dominant_emotion = emotion
+
+    emotion_scores["dominant_emotion"] = dominant_emotion
+    return emotion_scores
